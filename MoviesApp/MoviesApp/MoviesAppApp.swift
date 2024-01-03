b2c1b762b834248320ec20d011330241f6de99c4
lindaAdel <lindaadel17@gmail.com>
2024-01-03 18:41:37 +0200

    create Xcode Project

---
+++ MoviesApp/MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Linda adel on 03/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
